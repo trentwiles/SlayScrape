be62901e05570a661ca54133ea40bcc5e7033f58
RiversideRocks <wilest7@gmail.com>
2021-05-20 22:44:22 +0000

    Clean up code + begin write to json

--- main.py
+++ main.py
@@ -2,18 +2,26 @@ from bs4 import BeautifulSoup
 import requests
 import sys
 import json
+import os
 
+details = []
 site = sys.argv[1]
 r = requests.get(site, headers={"User-agent":"RiversideRocks"})
 soup = BeautifulSoup(r.text, 'html.parser') # removes warning
-#print(soup.prettify())
 title = soup.title.string
-print(title)
 
 for meta in soup.find_all('meta'):
     if str(meta.get("property")) == "None":
       print("Skipping meta tag as it is blank...")
+    elif str(meta.get("property")) != "og:description":
+      print("Skipping meta tag as it is wrong")
     else:
-      print(str(meta.get("content")) + " - " + str(meta.get("property")))
+      desc = str(meta.get("content"))
 
+json = open("result.json")
+details.append(site)
+details.append(title)
+details.append(desc)
+json.write(json.dumps(details))
+file.close()
 #print(soup.meta)
